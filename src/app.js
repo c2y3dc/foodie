a451b35e998946fc3bd167107c602050d41ea7be
Ervin Chow <ervinchow@gmail.com>
2016-06-25 21:27:44 -0700

    adds completed multi environment configuration

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const App = React.createClass({
 	render: function(){
 		return (
 				<div className={styles.wrapper}>
-					Hello World
+					<h1>Environment: {__NODE_ENV__}</h1>
 				</div>
 			)
 	}
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
+const webpack = require('webpack')
+const fs      = require('fs')
+const path    = require('path'),
+      join    = path.join,
+      resolve = path.resolve
+
+const getConfig = require('hjs-webpack')
+const root    = resolve(__dirname)
+const src     = join(root, 'src')
+const modules = join(root, 'node_modules')
+const dest    = join(root, 'dist')
+
 const NODE_ENV = process.env.NODE_ENV
 const dotenv = require('dotenv')
 const isDev = NODE_ENV === 'development'
@@ -18,20 +30,6 @@ const defines =
     __NODE_ENV__: JSON.stringify(NODE_ENV)
   })
 
-const webpack = require('webpack')
-const fs      = require('fs')
-const path    = require('path'),
-      join    = path.join,
-      resolve = path.resolve
-
-const getConfig = require('hjs-webpack')
-const root    = resolve(__dirname)
-const src     = join(root, 'src')
-const modules = join(root, 'node_modules')
-const dest    = join(root, 'dist')
-
-
-
 var config = getConfig({
   isDev: isDev,
   in: join(src, 'app.js'),
@@ -44,6 +42,10 @@ config.postcss = [].concat([
   require('autoprefixer')({}),
   require('cssnano')({})
 ])
+
+config.plugins = [
+  new webpack.DefinePlugin(defines)
+].concat(config.plugins)
 
 const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`
 const matchCssLoaders = /(^|!)(css-loader)($|!)/
